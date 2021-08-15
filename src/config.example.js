b757b3d81e4b94cc10024eba94e9b69f05e36cc2
Teo Oliver <teo.liz.oli@gmail.com>
2021-08-15 13:20:57 +0100

    add: config example file

---
+++ src/config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  websites: {
+    controlURL: 'https://www.xxx.com/uk',
+    testURL: 'https://www.yyy.com/uk',
+  },
+  outFileName: 'something-report',
+  reportsFolder: 'reports',
+  runLimit: 10,
+};
